b292009c3db87754483ef5a944a4c999c2e48cd2
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-03 23:12:17 +0700

    Implement all the worker features

--- index.js
+++ index.js
@@ -2,8 +2,56 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const https = require("https");
 
-const url = "http://edition.cnn.com/article/sitemap-2021-11.html";
+const baseURL = "https://edition.cnn.com";
+const yearURL = "https://edition.cnn.com/sitemap.html";
 
+const extractYearPage = ($) =>
+  $(".sitemaps-year-listing")
+    .find("div:nth-child(1) > ul > li")
+    .map((_, product) => {
+      const $product = $(product);
+      return {
+        year_link: $product.find("a").attr("href"),
+      };
+    })
+    .toArray();
+
+const subMonthsLink = ($, link) => {
+  const fullYearLink = baseURL + link;
+
+  axios.get(fullYearLink).then(({ data }) => {
+    const $ = cheerio.load(data);
+    const yearMonthLink = extractMonthsLink($);
+
+    for (var i = 0; i < yearMonthLink.length; i++) {
+      const fullMonthLink = baseURL + yearMonthLink[i]["link"];
+      called($, fullMonthLink);
+    }
+  });
+};
+
+const called = ($, link) => {
+  axios.get(link).then(({ data }) => {
+    const $ = cheerio.load(data);
+    const content = extractContent($);
+    // console.log(content);
+  });
+};
+
+const extractMonthsLink = ($) =>
+  $(".sitemap-month")
+    .find("li")
+    .map((_, product) => {
+      const $product = $(product);
+      const link = $product.find("a");
+      return {
+        link: link.attr("href"),
+        month: link.text(),
+      };
+    })
+    .toArray();
+
+// Date : Headline
 const extractContent = ($) =>
   $(".sitemap-entry")
     .find("ul > li")
@@ -18,8 +66,15 @@ const extractContent = ($) =>
     })
     .toArray();
 
-axios.get(url).then(({ data }) => {
+axios.get(yearURL).then(({ data }) => {
   const $ = cheerio.load(data);
-  const content = extractContent($);
-  console.log(content);
+
+  // { year_link: '/article/sitemap-2011.html' },
+  const yearShortLinks = extractYearPage($);
+  console.log(yearShortLinks);
+
+  for (var i = 0; i < yearShortLinks.length; i++) {
+    const yearFullLinks = yearShortLinks[i]["year_link"];
+    subMonthsLink($, yearFullLinks);
+  }
 });
