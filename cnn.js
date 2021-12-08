da7985364692d073704a488cd236d9ac323700b6
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-08 09:24:49 +0700

    Delete duplicate data

--- cnn.js
+++ cnn.js
@@ -32,6 +32,15 @@ connection.connect((err) => {
     }
     console.log("Create DB");
   });
+
+  let deleteDuplicate =
+    "DELETE t1 FROM news.cnn t1 INNER JOIN news.cnn t2 WHERE t1.id < t2.id AND t1.link = t2.link;";
+
+  connection.query(deleteDuplicate, function (err) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
 });
 
 const extractYearPage = ($) =>
