e56856e9879159587527d2fb08157e924b060b89
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-09 08:59:31 +0700

    Create a website for diplaying the data of CNN

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mysql = require("mysql");
+
+var cnnData;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "news",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+
+  let getData = "SELECT * FROM news.cnn;";
+
+  connection.query(getData, (err, result) => {
+    if (err) throw err;
+    cnnData = result;
+  });
+  connection.end();
+});
+
+app.get("/", function (req, resp) {
+  const welcomeText = "Welcome to EJS";
+  const favoriteThings = ["NYC", "Music", "Code", "Movies"];
+  const viewData = {
+    cnnData: cnnData,
+  };
+  resp.render("dataTemplate", viewData);
+});
+
+app.listen(3000);
