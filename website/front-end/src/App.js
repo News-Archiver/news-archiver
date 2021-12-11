399a42fa1bc85cd268a5d557c1b7db31e57c232c
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-11 10:47:34 +0700

    Remove React lazy

--- website/front-end/src/App.js
+++ website/front-end/src/App.js
@@ -1,27 +1,25 @@
-import React, { Suspense, useState } from "react";
+import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
+import FetchMoreData from "./components/FetchMoreData";
+import Main from "./components/Main";
 
 import "./App.css";
 
-const FetchMoreData = React.lazy(() => import("./components/FetchMoreData"));
-const Main = React.lazy(() => import("./components/Main"));
-
 function App() {
   const [cnnList, setCnnList] = useState([]);
+
   return (
     <>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <h1>🚀💥CNN Archive💥🚀</h1>
-        <hr />
-        <InfiniteScroll
-          dataLength={cnnList.length}
-          loadMore={FetchMoreData}
-          hasMore={true}
-          loader={<h2>Loading ...</h2>}
-        >
-          <Main cnnList={cnnList} setCnnList={setCnnList} />
-        </InfiniteScroll>
-      </Suspense>
+      <h1>🚀💥CNN Archive💥🚀</h1>
+      <hr />
+      <InfiniteScroll
+        dataLength={cnnList.length}
+        loadMore={FetchMoreData}
+        hasMore={true}
+        loader={<h2>Loading ...</h2>}
+      >
+        <Main cnnList={cnnList} setCnnList={setCnnList} />
+      </InfiniteScroll>
     </>
   );
 }
--- website/front-end/src/components/FetchMoreData.js
+++ website/front-end/src/components/FetchMoreData.js
@@ -1,16 +1,4 @@
-import { useState } from "react";
-
 const FetchMoreData = ({ cnnList, setCnnList }) => {
-  const [stateList, setState] = useState([
-    { cnnList: Array.from({ length: 20 }) },
-  ]);
-
-  setTimeout(() => {
-    setState({
-      items: stateList.cnnList.concat(Array.from({ length: 20 })),
-    });
-  }, 1500);
-
   return;
 };
 
