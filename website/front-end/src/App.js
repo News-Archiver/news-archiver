69ec1b815a7c84c83063166a134b5c43139b80fe
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-15 20:12:54 +0700

    Implement infinite scroll in React

--- website/front-end/src/App.js
+++ website/front-end/src/App.js
@@ -1,56 +1,49 @@
-import React, { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroller";
-// import FetchMoreData from "./components/FetchMoreData";
-import Axios from "axios";
-import Main from "./components/Main";
+import React, { useState, useRef, useCallback } from "react";
+import FetchMoreData from "./components/FetchMoreData";
 
 import "./App.css";
 
 function App() {
-  const [cnnList, setCnnList] = useState([]);
-  const [pageNum, setpageNum] = useState(1);
-  const [hasMore, sethasMore] = useState(true);
-  const [loaded, setLoaded] = useState(false);
+  const [pageNum, setPageNum] = useState(1);
 
-  console.log(pageNum);
+  const { loading, cnnList, hasMore } = FetchMoreData(pageNum);
 
-  const getQuery = () => {
-    if (!loaded) {
-      Axios.get(`http://localhost:3000/api/getCNN?page=${pageNum}`).then(
-        (data) => {
-          setCnnList(data.data);
-          console.log("data");
+  const observer = useRef();
+  const lastCnnElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNum((prevPageNum) => prevPageNum + 1);
         }
-      );
-      setLoaded(true);
-    }
-  };
-
-  useEffect(() => {
-    getQuery(pageNum);
-  });
-
-  const FetchMoreData = () => {
-    if (cnnList.length >= 1000) {
-      sethasMore(true);
-      setpageNum(pageNum + 1);
-      sethasMore(false);
-    }
-    return pageNum;
-  };
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   return (
     <>
       <h1>🚀💥CNN Archive💥🚀</h1>
       <hr />
-      <InfiniteScroll
-        dataLength={cnnList.length}
-        loadMore={FetchMoreData}
-        hasMore={hasMore}
-        loader={<h2>Loading ...</h2>}
-      >
-        <Main cnnList={cnnList} setCnnList={setCnnList} pageNum={pageNum} />
-      </InfiniteScroll>
+      {cnnList.map((val, key) => {
+        if (cnnList.length === key + 1) {
+          return (
+            <p ref={lastCnnElementRef} key={key}>
+              {key}
+              <a href={val.link}>{val.headline}</a>
+            </p>
+          );
+        } else {
+          return (
+            <p key={key}>
+              <a href={val.link}>{val.headline}</a>
+            </p>
+          );
+        }
+      })}
+      <div>{loading && "Loading..."}</div>
     </>
   );
 }
--- website/front-end/src/components/FetchMoreData.js
+++ website/front-end/src/components/FetchMoreData.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const FetchMoreData = () => {
-  const [pageNum, setpageNum] = useState(1);
-  setpageNum(pageNum + 1);
-  console.log(pageNum);
-  return pageNum;
+const FetchMoreData = (pageNum) => {
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
+  const [cnnList, setCnnList] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/api/getCNN?page=${pageNum}`)
+      .then((res) => {
+        setCnnList((prevCnnList) => {
+          return [...prevCnnList, ...res.data];
+        });
+        setHasMore(res.data.length > 0);
+        setLoading(false);
+      });
+  }, [pageNum]);
+
+  return { loading, cnnList, hasMore };
 };
 
 export default FetchMoreData;
