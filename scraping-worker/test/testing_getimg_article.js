14881757d18907cf440b243147f6c7b1e4d00cae
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-18 12:33:31 +0700

    Adding testing to the cnn fetching data bot

---
+++ scraping-worker/test/testing_getimg_article.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const prompt = require("prompt-sync")({ sigint: true });
+
+const url = prompt("URL: ");
+
+const getImage = ($) =>
+  $(".l-container")
+    .map((_, product) => {
+      const $product = $(product);
+      return {
+        img: `https:${$product.find("img").attr("data-src-small")}`,
+        alt: $product.find("img").attr("alt"),
+      };
+    })
+    .toArray();
+
+axios.get(url).then(async ({ data }) => {
+  let imgLink, imgAlt;
+  const $ = cheerio.load(data);
+  const content = await getImage($);
+  if (content.length === 0) {
+    imgLink = "https:undefined";
+    imgAlt = "undefined";
+  } else {
+    imgLink = content[0].img;
+    imgAlt = content[0].alt;
+  }
+  console.log(imgLink, imgAlt);
+});
---
+++ scraping-worker/test/testing_month.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const prompt = require("prompt-sync")({ sigint: true });
+
+const getImage = ($) =>
+  $(".l-container")
+    .map((_, product) => {
+      const $product = $(product);
+      return {
+        img: `https:${$product.find("img").attr("data-src-small")}`,
+        alt: $product.find("img").attr("alt"),
+      };
+    })
+    .toArray();
+
+const extractContent = ($, month) =>
+  $(".sitemap-entry")
+    .find("ul > li")
+    .map((_, product) => {
+      const $product = $(product);
+      const link = $product.find("a");
+      return {
+        link: link.attr("href"),
+        date: $product.find('span[class="date"]').text(),
+        headline: link.text(),
+        month: month,
+      };
+    })
+    .toArray();
+
+const callExtractContent = (link, month) => {
+  return axios.get(link).then(async ({ data }) => {
+    const $ = cheerio.load(data);
+    let save = await extractContent($, month);
+    let reg = /("|'|`)/gm;
+
+    for (let i = 0; i < save.length; i++) {
+      let { headline, link, date, month } = save[i];
+      let imgLink, imgAlt;
+      headline = headline.replace(reg, "'");
+      link = link.replace(reg, "'");
+      date = date.replace(reg, "'");
+      month = month.replace(reg, "'");
+
+      await axios.get(link).then(async ({ data }) => {
+        const $ = cheerio.load(data);
+        const content = await getImage($);
+        if (content.length === 0) {
+          imgLink = "https:undefined";
+          imgAlt = "undefined";
+        } else {
+          imgLink = content[0].img;
+          imgAlt = content[0].alt;
+        }
+      });
+
+      console.log(`Link ${link}`);
+      var sql = `INSERT INTO cnn (headline, link, date, month, imglink, imgalt) VALUES ("${headline}", "${link}", "${date}", "${month}", "${imgLink}", "${imgAlt}");`;
+      console.log(sql);
+
+      // saveToDB(headline, link, date, month, imgLink, imgAlt);
+    }
+  });
+};
+
+const url = prompt("URL: ");
+callExtractContent(url, "December");
